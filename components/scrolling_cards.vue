621e892d02df5e438dac56dc10d3f5b44a699907
Omar Yahia <oyahia@uci.edu>
2023-09-20 13:08:35 -0700

    infinite animation

---
+++ components/scrolling_cards.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="grid">
+        <div class="content" v-for="member in member_data">
+            <MemberCard class="card" :member="member"></MemberCard>
+        </div>
+        <div class="duplicated_content" v-for="member in member_data">
+            <MemberCard class="card" :member="member"></MemberCard>
+        </div>
+    </div>
+</template>
+
+<script>
+import MEMBER_DATA from '../member_data.json';
+export default{
+    data(){
+        return{
+            member_data: MEMBER_DATA
+        }
+    }
+}
+</script>
+
+<style scoped>
+@keyframes scroll {
+	0% { transform: translateX(calc(-300px * 5))}
+    100% { transform: translateX(calc(0))}
+}
+.grid { 
+  animation: scroll 20s linear infinite;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-rows: auto auto;
+  grid-gap: 20px;
+  align-items: start;
+  height: 600px;
+}
+h1 {
+    margin-top: 50px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 
 <template>
-  
+  <ScrollingCards></ScrollingCards>
   <div className="announcements">
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTz=1&showTabs=0&showPrint=1&showDate=1&showNav=0&mode=AGENDA&showCalendars=0&src=MzE0NDMxZTFhMzNlYmQ1MGM1MTVmODM5MmQxNjg4MDA4MDA5ZGE1MjMyNzMyZTY3ZjM5MzZlMjJlNGQzZTYyMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E" style="border-width:0" width="800" height="400" frameborder="0" scrolling="no"></iframe>
   </div>
