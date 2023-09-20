f58c6a11d3b966c7fefac0abc3aea00de3216aa1
Omar Yahia <oyahia@uci.edu>
2023-09-20 11:51:12 -0700

    worked on members page

--- components/member_card.vue
+++ components/member_card.vue
@@ -1,8 +1,40 @@
 <template>
-    <div class="card">
-        {{ member.name }}
-    </div>
+    <a :href="member.website"> 
+        <div class="card">
+            <img :src="'/club_logos/' + member.logo" alt="pic">
+            <h2>{{ member.name }}</h2>
+        </div>
+    </a>
 </template>
+
+<style scoped>
+    .card{
+        position: relative;
+        text-align: center;
+        width: 300px;
+        height: 200px;
+        border-radius: 50px;
+        background-color: var(--frameColor);
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #ccc;
+        box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+    }
+    img{
+        width: 300px;
+        height: auto;
+    }
+    h2{
+        color: black;
+        text-decoration: none;
+        position: absolute;
+        width: 50%;
+        top: 50;
+        bottom: 50;
+    }
+</style>
 
 <script>
 export default {
--- pages/members.vue
+++ pages/members.vue
@@ -1,8 +1,26 @@
 <template>
-    <div class="grid" v-for="member in member_data">
-        <MemberCard :member="member"></MemberCard>
+    <h1>Current Members</h1>
+    <div class="grid">
+        <div class="content" v-for="member in member_data">
+            <MemberCard class="card" :member="member"></MemberCard>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.grid { 
+  margin: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  align-items: start;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+    margin-top: 50px;
+}
+</style>
 
 <script>
 import MEMBER_DATA from '../member_data.json';
