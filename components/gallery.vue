ebf2e52f5ce42a4bd0c949d809d3d04323df266e
Omar Yahia <oyahia@uci.edu>
2023-09-20 18:36:46 -0700

    gallery

---
+++ components/gallery.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="gallery">
+        {{ caption }}
+        <div class="image-carousel">
+            <div class="imgContainer">
+                <transition-group name="fade" tag="div" appear>
+                    <img
+                    v-for="(picture, index) in pictures"
+                    :key="index"
+                    :src="'/' + folder + '/' + picture" alt="picture"
+                    :style="{ zIndex: index === currentIndex ? 1 : 0 }"
+                    class="carousel-image"
+                    v-show="index === currentIndex"
+                    />
+                </transition-group>
+            </div>
+            <div class="carousel-controls">
+                <Icon name="ic:baseline-arrow-left" @click="prevImage" color="white" size="50px" class="carousel-arrow left">←</Icon>
+                <Icon name="ic:baseline-arrow-right" @click="nextImage" color="white" class="carousel-arrow right" size="50px">←</Icon>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  props: ['pictures', 'folder', 'caption'],
+  data() {
+      return {
+        currentIndex: 0,
+      };
+    },
+    methods: {
+        nextImage() {
+            this.currentIndex = (this.currentIndex + 1) % this.pictures.length;
+        },
+        prevImage() {
+            this.currentIndex = this.currentIndex === 0 ? this.pictures.length - 1 : this.currentIndex - 1;
+        },
+    },
+}
+</script>
+  
+<style scoped>
+  .carousel-controls {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    z-index: 2;
+    position: relative;
+  }
+
+  .carousel-arrow {
+    background: rgba(0, 0, 0, 0.5);
+    border: none;
+    width: 50px;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+
+  .image-carousel {
+    background-color: var(--frameColor);
+    position: relative;
+    width: 500px;
+    height: 500px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .imgContainer {
+    margin: 5%;
+    overflow: hidden;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    height: 90%;
+  }
+
+  .carousel-image {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    min-height: 100%;
+    opacity: 1;
+  }
+  
+    .fade-enter-active, .fade-leave-active {
+        transition: all 0.5s ease-out;
+    }
+
+    .fade-enter-from, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
-<!-- I made a different home page component because for some reason css was not loading on initial render-->
 <template>
   <ScrollingCards></ScrollingCards>
   <div className="mission">
-      <h1>The Cal Animage Network (CAN) is one of the largest Japanese animation, or anime club communities, including thousands of members all over the world. Enroll with your club now to be among countless passionate anime aficionados across the globe.
-</h1>
+      <h1>The Cal Animage Network (CAN) is one of the largest Japanese animation, or anime club communities, including thousands of members all over the world. Enroll with your club now to be among countless passionate anime aficionados across the globe.</h1>
   </div>
   <div className="announcements">
       <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTz=1&showTabs=0&showPrint=1&showDate=1&showNav=0&mode=AGENDA&showCalendars=0&src=MzE0NDMxZTFhMzNlYmQ1MGM1MTVmODM5MmQxNjg4MDA4MDA5ZGE1MjMyNzMyZTY3ZjM5MzZlMjJlNGQzZTYyMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F4511E" style="border-width:0" width="800" height="400" frameborder="0" scrolling="no"></iframe>
   </div>
-  
+  <h1>Galleries</h1>
+  <div class="galleries">
+    <Gallery :pictures="picture_data.art" folder="artwork" caption="Artwork"></Gallery>
+    <Gallery :pictures="picture_data.events" folder="event_art" caption="Events"></Gallery>
+  </div>
   <div class="map">
       <iframe src="https://www.google.com/maps/d/embed?mid=1nRp4RbVJwb9nQHRbhoUVLtzoy20qd44&ehbc=2E312F" width="640" height="480"></iframe>
   </div>
@@ -30,4 +32,26 @@
   width: 80%;
   height: 100%;
 }
+h1 {
+  margin-top: 50px;
+}
+.galleries{
+  margin-bottom: 50px;
+  margin-left: 10%;
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
 </style>
+
+<script>
+import PICTURE_DATA from '../gallery_data.json';
+export default{
+    data(){
+        return{
+            picture_data: PICTURE_DATA
+        }
+    }
+}
+</script>
