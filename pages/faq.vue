077a8131925d8404731d7187b6055face4a070d4
Omar Yahia <oyahia@uci.edu>
2023-12-30 09:25:56 -0800

    finished faq from checklist, made history consistent

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,13 +1,146 @@
 <template>
     <h1>FAQ</h1>
     <div class="faqBox">
-        <h2>What is Cal Animage?</h2>
-        <p>Cal Animage is a loosely organized association of International Anime clubs. CAA is one of the oldest anime clubs in North America and one of the largest anime clubs in the world outside of Japan.</p>
+        <h2>General Questions</h2>
 
-        <h2>What was the first chapter in Cal Animage?</h2>
-        <p>Cal Animage Alpha (CAA) was the first chapter of Cal Animage founded in 1989 by Mike Tatsugawa, George Herbert, Albert Wang and Mike Ellis. CAA was the University of Califonia at Berkley's (UC Berkley) anime club. Tatsugawa was the founding president.</p>
+            <div class="question">
+                <p><b>What is the Cal Animage Network?</b></p>
+                <p>We are an association of anime clubs that started in 1989! Our first club was Cal Animage Alpha, which was founded at UC Berkeley. Clubs originally followed a Greek-letter naming style, with each additional club representing another ‘chapter’ in Animage. Read more about our history <a href="https://animage.moe/history">here!</a></p>
+            </div>
 
-        <h2>...</h2>
+            <div class="question">
+                <p><b>What does the Cal Animage Network do?</b></p>
+                <p>Shortly put, we are here to help you <b>connect and collaborate.</b></p>
+                <p>We want to connect fellow anime clubs together under a common umbrella so that we can share and cooperate with one another. We help anime clubbers exchange ideas, share club materials, and offer expertise on how to better run our clubs and grow our communities.</p>
+                <p>Do you need resources for your clubs? Need some ideas for your club meetings? No idea how to run a maid cafe? Curious what your neighboring anime clubs are up to? Distance should not be an obstacle for helping other anime clubbers out. So let’s head into this together.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Does the Network host in-person events?</b></p>
+                <p>Although the majority of our inter-club work is done online, where it is easy to connect and work with other anime clubbers, our collaborations are not exclusively online. We provide support to in-person events hosted by our clubs and affiliates, and we aim to help organize more and more events in-person!</p>
+            </div>
+
+            <div class="question">
+                <p><b>Does the Animage Network have any relation to the Japanese anime and entertainment magazine named ‘Animage’?</b></p>
+                <p>No relation.</p>
+            </div>
+
+            <div class="question">
+                <p><b>How do I join the Network (as an individual)? Do I have to be part of a club to join the Network?</b></p>
+                <p>The Animage Network is geared more towards uniting club organizations and fostering their interactions with each other, so individual human beings generally do not need to be registered except for specific roles, like staff-member, club officer, or delegate.</p>
+                <p>So whether you are a member of an Animage club or not, everyone is welcome to participate in our public spaces as a guest or affiliate. To keep in touch, we encourage you to subscribe to our <a href="/email">mailing list</a>!</p>
+            </div>
+
+            <div class="question">
+                <p><b>I’m not a member of any club in Animage. Can I join one, or attend their meetings?</b></p>
+                <p>Each club has their own rules on club membership and who can attend their meetings. You will need to reach out to them on your own and adhere to their boundaries. Generally, campus-based clubs usually only allow students from their own campus.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Are club alumni allowed in the Network (from campus-orientated clubs)?</b></p>
+                <p>Yes! For most purposes, as long as you are in good standing with the club you graduated from, it makes no difference to the Network whether or not you are a current student or former student.</p>
+            </div>
+
+            <div class="question">
+                <p><b>What is a Network Staffer? How can I become one?</b></p>
+                <p>Network staffers are individuals that have been on-boarded for the explicit purpose of helping to run the Network and its components. Depending on the role, we generally try to recruit from those that we know or those that have a good history in an Animage club. You should be able to apply through our Discord server.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Can my organization still affiliate or collaborate with the Network if it is not an anime club?</b></p>
+                <p>Yes, most definitely! We are open to partnering and collaborating with organizations that resonate with our Network’s purpose. This can include organizations like convention hosts, maid cafes, and sellers of art and merchandise. Please reach out to us on Discord or through email. As a starter, partnerships with existing Animage clubs are always favorable, and the Network can expect to be publicly listed as a sponsor.</p>
+            </div>
+
+        <h2>Club-Related Questions</h2>
+
+            <div class="question">
+                <p><b>Does it cost money to join the Network? What does it offer?</b></p>
+                <p>The Network charges $100 per club registration. Just kidding, no we don’t. It’s $0.00. It doesn’t cost anything. <b>Joining is free!</b></p>
+                <p>Benefits of Network membership for a club include being:</p>
+                <ul>
+                    <li>Displayed on our website and Network map among other members.</li>
+                    <li>Listed as a Network member in the Network’s monthly newsletter</li>
+                    <li>Granted the privilege of sharing free advertisements through our Network’s mailing list and newsletter.</li>
+                    <li>Granted exclusive collaborative access to privately shared materials, channels, and resources for Animage clubs.</li>
+                </ul>
+            </div>
+
+            <div class="question">
+                <p><b>How does my club apply to join the Network? What are the requirements?</b></p>
+                <p>Your club may apply (to register) through Google Forms at <a href="/join">https://animage.moe/join</a></p>
+                <p>Our charter requires that all clubs:</p>
+                <ol>
+                    <li>Be an in-person club with membership that is exclusive and regulated, with a primary location of in-person residence (e.g. your campus).</li>
+                    <li>Have a public main web or social-media page.</li>
+                    <li>Display an official original club logo or mascot.</li>
+                </ol>
+                <p>If your club adheres to all of these requirements, we will review your application and accept on a case-by-case basis. </p>
+            </div>
+
+            <div class="question">
+                <p><b>How does registration and re-registration work? Is there a deadline?</b></p>
+                <p>It’s easy! Clubs apply to register or re-register through the exact same process as if they were applying at <a href="/join">https://animage.moe/join</a>. The Network registers clubs on a rolling basis, meaning that clubs can apply and may be accepted anytime throughout the year.</p>
+                <p>However, November 1st is the annual deadline for continuous re-registration. Clubs need to re-register and provide updated information every year in order to stay in the Network. Clubs may start re-registering as early as August.</p>
+                <p>If your club misses re-registration, it will simply not be a membering club until it re-registers. Its membership will be considered interrupted.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Does my club need to be in California to join the Network?</b></p>
+                <p>Nope. We welcome clubs from all over the world! Some of Cal Animage’s earliest clubs were from far outside California, in fact. We consider ourself a global network that happens to be rooted from California and its especially robust anime scene.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Can my club join the Network if it is not exclusively an anime club (i.e. hybrid)? For example, if it were nominally an anime-gaming club, an or anime-comic club.</b></p>
+                <p>We will judge on a case-by-case basis, but an element of Japanese animation will need to be a strongly predominant feature of your club. Features such as having an anime mascot, or regularly watching anime, are good signs for qualification.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Can my anime club join if it is not campus-based (from a school)?</b></p>
+                <p>Yes. The Network has the most experience dealing with campus-based clubs. This is most common, but it is technically not a requirement.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Can my anime club join if it is from a high school?</b></p>
+                <p>Unfortunately, our <a href="/docs">charter</a> requires that Animage clubs must be composed foremostly of “legal-aged adults”. However, there may yet still be opportunities for junior-level clubs to contribute to the Network.</p>
+            </div>
+
+        <h2>Being Part of the Network</h2>
+
+            <div class="question">
+                <p><b>What is required from clubs while they are in the Network?</b></p>
+                <p>Unless your club goes out of the way to participate in the Network, <b>membership in the Network is very low maintenance</b>! Officers are only encouraged, not required, to participate in the Network on a routine basis.</p>
+                <p>Our <a href="/docs">charter</a> only requires that clubs must:</p>
+                <ol>
+                    <li>Exhibit Network membership on their main online page, with <a href="/">https://animage.moe/</a> linked.</li>
+                    <li>Allow the Network to share Network-related information in club spaces.</li>
+                    <li>Not violate any specific Network by-laws that may be in place.</li>
+                    <li>Be inclusive and friendly with other clubs, and themselves.</li>
+                </ol>
+                <p>Other than these basic rules, the Network has little business telling clubs how to run themselves.</p>
+            </div>
+
+            <div class="question">
+                <p><b>What authority will the Network have over my club?</b></p>
+                <p>The Network has no authority to run any club or its internal affairs. Each club manages itself. If a member club fails to adhere to Network guidelines, the Network can only regulate or remove the club’s membership and privileges in the Network.</p>
+            </div>
+
+            <div class="question">
+                <p><b>Who runs the Network?</b></p>
+                <p>The Network is run under the authority of several <b>Entrusted Clubs</b>, which are Animage clubs that have been specially entrusted to steer the Network. Each Entrusted Club appoints a representative delegate of their choosing to the <b>Network Delegation</b>, which serves as the ultimate governing body of Animage (like a legislature, but simpler and more agreeable and more anime). Delegates are intended to represent their clubs by formally voting on whether the Delegation should or should not put certain measures into action (e.g. to host a certain event, or make an official statement on behalf of the entire Network).</p>
+                <p>An important detail: a high threshold of 3/4ths in favor is required for any vote by the Delegation to pass, so it is incredibly unlikely that the Network would commit to something that most clubs would find displeasing.</p>
+            </div>
+
+            <div class="question">
+                <p><b>How can a club become an Entrusted Club?</b></p>
+                <p>The <a href="/docs">charter</a> dictates that for a club to become entrusted, it must:</p>
+                <ul>
+                    <li>Be distinguishably active (to similar levels of current Entrusted Clubs)</li>
+                    <li>Be recommended for entrustment by one current entrusted club</li>
+                    <li>Currently be in the Network for approximately at least 2 consecutive years, OR be a former entrusted club of at least 4 consecutive years, OR be among the original Cal Animage clubs founded in California from 1989 to 1994.</li>
+                </ul>
+                <p>This is a measure to ensure that Entrusted Clubs are stable and reputable enough to steer the Network in the long term. But while some clubs may have to wait for some time before they can become entrusted, the Network can also confer individuals of non-entrusted clubs as co-delegates to represent their clubs. Co-delegates cannot vote, but can help contribute by providing commentary and suggestions in the same space as other delegates.</p>
+            </div>
+
     </div>
 
 </template>
@@ -29,10 +162,21 @@
     }
     h2{
         font-size: 30px;
-        @media screen and (max-width: 700px) {
-            width: 100%;
-            text-align: center;
-        }
+        width: 100%;
+        text-align: center;
+    }
+    ol {
+        list-style-type: none; /* Remove default numbering style */
+        counter-reset: my-counter; /* Reset the counter for custom numbering */
+    }
+
+    ol li {
+        counter-increment: my-counter; /* Increment the counter for each list item */
+    }
+
+    /* Style for the pseudo-element ::before to create custom numbering */
+    ol li::before {
+        content: counter(my-counter) ") "; /* Use parentheses for numbering */
     }
     
 </style>
--- pages/history.vue
+++ pages/history.vue
@@ -2,13 +2,15 @@
     <h1>History</h1>
     <div class="historyBox">
         <h2>WIP</h2>
-        <p>
-            The history page of Cal Animage is a work in progress! See
-            <a href="https://www.animenewsnetwork.com/encyclopedia/lexicon.php?id=114">https://www.animenewsnetwork.com?/encyclopedia/lexicon.php?id=114</a> 
-            and <a href="https://apricot.com/~shogun/C-A.html">https://apricot.com/~shogun/C-A.html</a> 
-            for old sources overviewing the origins of Cal Animage.
-        </p>
-        <p>Do you know something about Cal Animage’s past that you could help tell the world? Please contact us, and help us tell the story of Animage’s early anime clubs!</p>
+        <div class="history">
+            <p>
+                The history page of Cal Animage is a work in progress! See
+                <a href="https://www.animenewsnetwork.com/encyclopedia/lexicon.php?id=114">https://www.animenewsnetwork.com?/encyclopedia/lexicon.php?id=114</a>
+                and <a href="https://apricot.com/~shogun/C-A.html">https://apricot.com/~shogun/C-A.html</a>
+                for old sources overviewing the origins of Cal Animage.
+            </p>
+            <p>Do you know something about Cal Animage’s past that you could help tell the world? Please contact us, and help us tell the story of Animage’s early anime clubs!</p>
+        </div>
     </div>
 </template>
 
@@ -28,10 +30,8 @@
     }
     h2{
         font-size: 30px;
-        @media screen and (max-width: 700px) {
-            width: 100%;
-            text-align: center;
-        }
+        width: 100%;
+        text-align: center;
     }
     p{
         width: 100%;
